754c5a282a805265c24e474316c63664e8cc4ba5
Tom May <tom@tommay.net>
2015-10-22 09:39:02 -0700

    Add bare metal init code to setup().

--- Spiro.ino
+++ Spiro.ino
@@ -1,5 +1,20 @@
+/*
+  Pin usage:
+
+  PC0 - PC5: analog in
+  PD6 - PD7: encoder in
+  PB2: nSS out (DAC chip select)
+  PB1: nLDAC out (DAC latch load)
+
+  PB2 - PB5: SPI
+  PB6 - PB7: xtal
+  PC6: nRESET
+*/
+
 #pragma GCC optimize ("-O2")
 
+#include <avr/io.h>
+#include <avr/cpufunc.h>	// _NOP
 #include <stdint.h>
 #include "fixed_point.h"
 #include "zsin.h"
@@ -38,6 +53,34 @@ static unsigned long then;
 void
 setup() {
   Serial.begin(9600);
+
+  // SPI.  Enable as master, set mode, clk = fOsc/2 (max).
+
+  SPCR = _BV(SPE) | _BV(MSTR) | _BV(CPHA) | _BV(CPOL);
+  SPSR = _BV(SPI2X);
+
+  // Encoder.  Pins PD6 and PD7 are input (default) with pull-ups enabled.
+
+  PORTD = _BV(PORTD6) | _BV(PORTD7);
+
+  // Knobs (PC0-5/ADC0-5) are input (default) with pull-ups disabled
+  // (default) and digital input buffers disabled.
+
+  DIDR0 |= _BV(ADC0D)|_BV(ADC1D)|_BV(ADC2D)|_BV(ADC3D)|_BV(ADC4D)|_BV(ADC5D);
+
+  // PB2/nSS is the DAC chip select.  Output high.
+
+  DDRB |= _BV(DDB2);		// PB2 is output.
+  PORTB |= _BV(PORTB2);		// PB2 is high.
+
+  // PB1 is nLDAC, the DAC latch.  Output high.
+
+  DDRB |= _BV(DDB1);		// PB1 is output.
+  PORTB |= _BV(PORTB1);		// PB1 is high.
+
+  // Enable pull-ups on unused/floating input pins.
+
+  // XXX PORTB |= _BV(PB1) | _BV(PB2) | _BV(PB5);
 }
 
 void
